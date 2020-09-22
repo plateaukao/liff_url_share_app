500171edca70364100b5fd3a99e822de2eda0b73
Daniel Kao <daniel.kao@gmail.com>
2020-09-23 01:31:25 +0800

    first commit

---
+++ app/src/main/java/info/plateaukao/liffurlpreviewsharer/MainActivity.kt
@@ -0,0 +1,29 @@
+package info.plateaukao.liffurlpreviewsharer
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.URLEncoder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (intent?.action == Intent.ACTION_SEND) {
+            handleSendText(intent) // Handle text being sent
+        }
+        else {
+        }
+    }
+
+    private fun handleSendText(intent: Intent) {
+        val sharedUrl = intent.getStringExtra(Intent.EXTRA_TEXT)
+        var liffUrl = "https://liff.line.me/1654950578-67erzKdm"
+        liffUrl += "?url=" + URLEncoder.encode(sharedUrl);
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(liffUrl))
+        startActivity(webIntent)
+        finish()
+    }
+}
