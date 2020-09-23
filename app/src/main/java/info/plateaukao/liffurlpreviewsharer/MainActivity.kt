0032db76b8ffcf895eccc139e3befa88ac7211e3
Daniel Kao <daniel.kao@gmail.com>
2020-09-23 09:14:42 +0800

    try to get copy data in clipboard

--- app/src/main/java/info/plateaukao/liffurlpreviewsharer/MainActivity.kt
+++ app/src/main/java/info/plateaukao/liffurlpreviewsharer/MainActivity.kt
@@ -1,27 +1,42 @@
 package info.plateaukao.liffurlpreviewsharer
 
+import android.content.ClipboardManager
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputTextField: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inputTextField = findViewById<EditText>(R.id.textInputEditText)
+        val shareButton = findViewById<Button>(R.id.button_share)
+
+        shareButton.setOnClickListener { _ ->
+            handleSendText(inputTextField.text.toString())
+        }
+
         if (intent?.action == Intent.ACTION_SEND) {
-            handleSendText(intent) // Handle text being sent
+            var url = intent.getStringExtra(Intent.EXTRA_TEXT) ?: ""
+            handleSendText(url) // Handle text being sent
         }
-        else {
-        }
+
+
     }
 
-    private fun handleSendText(intent: Intent) {
-        var sharedUrl = intent.getStringExtra(Intent.EXTRA_TEXT)
-        if (sharedUrl?.startsWith("http") != true) {
-            sharedUrl = sharedUrl?.substring(sharedUrl.indexOf("http"))
+    private fun handleSendText(url: String) {
+        var sharedUrl = url
+        if (sharedUrl.isEmpty()) return
+
+        if (!sharedUrl.startsWith("http")) {
+            sharedUrl = sharedUrl.substring(sharedUrl.indexOf("http"))
         }
 
         var liffUrl = "https://liff.line.me/1654950578-67erzKdm"
@@ -29,5 +44,16 @@ class MainActivity : AppCompatActivity() {
         val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(liffUrl))
         startActivity(webIntent)
         finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // what the heck!
+        val clipBoardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        val copiedString = clipBoardManager.primaryClip?.getItemAt(0)?.toString() ?: ""
+        if (copiedString.isNotEmpty()) {
+            inputTextField.setText(copiedString)
+        }
     }
 }
